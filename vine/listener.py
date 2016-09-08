d31780403690df60fb767c6346f471a95ec2a7aa
Oswald <oswald.capriles46@gmail.com>
2016-09-07 23:59:17 -0400

    Fixed some things in listener module

--- vine/listener.py
+++ vine/listener.py
@@ -22,47 +22,75 @@ class Socket_Process(mp.Process):
 
 	def __init__(self, id="Listen_Process", sock=None):
 		super(Socket_Process, self).__init__(name=id)
-		self._sock = new_socket() if not sock else sock
+		self._sock = ListenSocket()
 		self.SCRIPT_STATUS = mp.Value('i', 1)
 
 	def run(self):
-		
 		while True:
-
+			self._sock.wait()
 			try:
-				print '\n Waiting for a connection \n'
-				c, addr = self._sock.accept()
-			except:
-				exit("\n- Finished socket process")
-
-			try:
-				print '\n Connection from', addr, '\n'
-				while True:
-					msg = c.recv(1024)
-					if not msg: break
-					print '\n Message:', msg, '\n'
-					msg = msg.lower()
-					if msg == 'change status':
-						self.SCRIPT_STATUS.value = not self.SCRIPT_STATUS.value
-					elif msg == 'get status':
-						c.sendall(str(self.SCRIPT_STATUS.value))
+				msg = self._sock.recv()
+				print '-> Message:', msg	
+				self.interpret_msg(msg)()
 			finally:
-				c.close()
-	
+				self._sock.disconnect()
+
+	def toggle_status(self):
+		new_status = not self.SCRIPT_STATUS.value
+		self.SCRIPT_STATUS.value = new_status
+
+	def get_status(self):
+		status = str(self.SCRIPT_STATUS.value)
+		self._sock.send(status)
+
 	def stop(self):
-		_sock.close()
+		self._sock.close()
 		self.terminate()
 		# Should i call join?
 
-def new_socket():
-	clear_socket_path()
-	sock = socket.socket(
-		socket.AF_UNIX, socket.SOCK_STREAM)
-	sock.bind(SOCKET_PATH)
-	sock.listen(MAX_CLIENTS)
-	sp.call([CONFIGURE_SCRIPT])
-	return sock
+	def interpret_msg(self, msg):
+		msg = msg.upper()
+		return {
+			'CHANGE STATUS': self.toggle_status,
+			'GET STATUS': self.get_status
+		}[msg]
 
+class ListenSocket:
+	
+	def __init__(self, sock=None):
+		clear_socket_path()
+		self._sock = socket.socket(
+			socket.AF_UNIX, socket.SOCK_STREAM)
+		self._sock.bind(SOCKET_PATH)
+		self._sock.listen(MAX_CLIENTS)
+		sp.call([CONFIGURE_SCRIPT])
+		self._conn = None
+
+	def wait(self):
+		print 'Waiting for a connection \n'
+		self._conn = self._sock.accept()[0]
+
+	def recv(self, buff=1024):
+		if self._conn:
+			msg = self._conn.recv(buff)
+		return msg
+
+	def send(self, msg):
+		if self._conn:
+			self._conn.sendall(msg)
+
+	def disconnect(self):
+		if self._conn:
+			self._conn.close()
+			self._conn = None
+
+	def close(self):
+		self.disconnect()
+		if self._sock: 
+			self._sock.close()
+			clear_socket_path()
+
+# Helper
 def clear_socket_path():
 	if not os.path.exists(SOCKET_FOLDER):
 		sp.call(['mkdir', '-p', SOCKET_FOLDER])
@@ -74,9 +102,11 @@ def clear_socket_path():
 				raise
 
 if __name__ == '__main__':
+
+	# Listener Test
 	
-	sp = Socket_Process()
-	sp.start()
+	test = Socket_Process()
+	test.start()
 
 	try: 
 		while 1: pass # Butt loop
